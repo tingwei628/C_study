95334cc345c7f5d78e979c446d3f79e289beeed0
陳廷維 <sbvq6p199xe@gmail.com>
2017-01-22 23:28:27 +0800

    add how to write a makefile

---
+++ examples/write_a_makefile/h.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+#include "hello.h"
+int main() {
+ /* int a, b, temp;
+  printf("input a number\n");
+  scanf("%d", &a);
+  scanf("%d", &b);
+  while((a % b)!= 0) {
+    temp = a % b;
+    a = b;
+    b = temp;
+  }
+  printf("%d\n", b);
+  */
+  hello("world");
+  return 0;
+}
