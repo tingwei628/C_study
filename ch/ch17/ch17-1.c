acc07a34aa594b6515cbaaa0771638037f74fb5e
tingwei628 <sbvq6p199xe@gmail.com>
2017-12-16 00:24:10 +0800

    add sorted linked list and insert method

--- ch/ch17/ch17-1.c
+++ ch/ch17/ch17-1.c
@@ -17,30 +17,58 @@
     
     //NOTE: malloc return void *, it is undefined type pointer
 */
+
+//iterate
+Node *insert(int data, Node *head)
+{
+  Node *ptr = (Node *)malloc(sizeof(Node));
+  Node *prev;
+  Node *curr;
+  // from start
+  if (head == NULL || head->num <= data)
+  {
+    ptr->num = data;
+    ptr->next = head;
+    head = ptr;
+    return head;
+  }
+
+  curr = head;
+  while (curr->num > data)
+  {
+    prev = curr->next;
+    if (prev == NULL || prev->num <= data)
+    {
+      ptr->num = data;
+      ptr->next = prev;
+      curr->next = ptr;
+      break;
+    }
+    else
+    {
+      curr = prev;
+    }
+  }
+  return head;
+}
 void sortlinkedlist()
 {
   // head cannot be moved
   // the end node of linked list is prefered as NULL
-  Node *head;
-  Node *prev = NULL;
-  int a[5] = {1, 2, 3, 4, 5};
+  Node *head = NULL;
+  int a[5] = {2, 5, 1, 3, 4};
   /*
-    head prev                    
-    [5]->[4]->[3]->[2]->[1]->NULL
+    head  prev                    
+          [5]->[4]->[3]->[2]->[1]->NULL
   */
   int i = 0;
   while (i < 5)
   {
-
-    // get ptr next pointer and assign to ptr
-    head = (Node *)malloc(sizeof(Node));
-    head->num = a[i];
-    head->next = prev;
-    prev = head; // give new address to prev, it means "move prev"
+    head = insert(a[i], head);
     i++;
   }
 
-  //print linked list
+  //print sortlinked list
   for (; head != NULL; head = head->next)
   {
     printf("num: %d\n", head->num);
